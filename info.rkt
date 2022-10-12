94e4226f5dd9d860f9ac922a7bbbd91553fa0b85
homedirectory <dalvkylib@gmail.com>
2022-10-12 20:50:06 +0300

    Files for package installation.

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define collection "rackup")
+(define version "2.0")
+(define deps '("base"))
+(define pkg-desc "Backup utility with nice features")
+(define license '(GPL-3.0-or-later))
+(define pkg-authors '(homedirectory))
---
+++ main.rkt
@@ -0,0 +1,4 @@
+#lang racket/base
+
+(require (file "src/cli.rkt"))
+(provide (all-from-out (file "src/cli.rkt")))
